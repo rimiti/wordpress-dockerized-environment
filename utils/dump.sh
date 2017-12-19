21c9f393dd25c89efee642707e7abf01db33ebe9
rimiti <dimitri.dobairro@dimsolution.com>
2017-12-19 18:56:27 +0100

    chore(utils/dump.sh) script to dump mysql container

---
+++ utils/dump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+_os="`uname`"
+_now=$(date +"%m_%d_%Y")
+_file="src/data_$_now.sql"
+docker-compose exec mydb.mysql.db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"' > $_file
+if [[ $_os == "Darwin"* ]] ; then
+  sed -i '.bak' 1,1d $_file
+else
+  sed -i 1,1d $_file # Removes the password warning from the file
+fi
